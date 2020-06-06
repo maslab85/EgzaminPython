980807ffa6a6be967daf938a446f1a5aa4e9d60c
maslab85 <piotr.p.balcerzak@gmail.com>
2020-06-06 22:40:42 +0200

    Ver 2

--- egzaminpy/egzaminpy.py
+++ egzaminpy/egzaminpy.py
@@ -36,39 +36,55 @@ def wysz_znak(tekst):
 
 print(wysz_znak("ala"))
 
-def szukaj(slowo):
-    slownik = {    "sto":"1",
-                   "dwiescie": "2",
-                   "trzysta": "3",
-                   "czterysta": "4",
-                   "piencet": "5",
-                   "szescset": "6",
-                   "siedemset": "7",
-                   "osiemset": "8",
-                   "dziewiecet": "9",
-                   "dziesiec":"1",
-                   "dwadziescia": "2",
-                   "trzydziesci":"3",
-                   "czterdziesci":"4",
-                   "piedziesiat":"5",
-                   "szescdziesiąt":"6",
-                   "siedemdziesiat":"7",
-                   "osiemdziesiat": "8",
-                   "dziewiedziesiat":"9",
-                   "zero":"0",
-                   "jeden": "1",
-                   "dwa":"2",
-                   "trzy":"3",
-                   "cztery":"4",
-                   "piec":"5",
-                   "szejsc":"6",
-                   "siedem":"7",
-                   "osiem":"8",
-                   "dziewiec":"9"
+def szukaj(liczba):
+    slownik = {    "sto":1,
+                   "dwiescie": 2,
+                   "trzysta": 3,
+                   "czterysta": 4,
+                   "piencet": 5,
+                   "szescset": 6,
+                   "siedemset": 7,
+                   "osiemset": 8,
+                   "dziewiecet": 9,
+                   "dziesiec":1,
+                   "dwadziescia": 2,
+                   "trzydziesci":3,
+                   "czterdziesci":4,
+                   "piedziesiat":5,
+                   "szescdziesiąt":6,
+                   "siedemdziesiat":7,
+                   "osiemdziesiat": 8,
+                   "dziewiedziesiat":9,
+                   "zero":0,
+                   "jeden": 1,
+                   "dwa":2,
+                   "trzy":3,
+                   "cztery":4,
+                   "piec":5,
+                   "szejsc":6,
+                   "siedem":7,
+                   "osiem":8,
+                   "dziewiec":9
                    }
-   
-    if slowo in slownik:
-        return slownik[slowo]
+    slowo=[]
+    c=0
+
+    for a in range(len(liczba)):
+        
+        if liczba[a]==" ":
+            slowo.append(liczba[c:a])
+            c=a+1
+        if a==(len(liczba)-1):
+            slowo.append(liczba[c:a+1])
+            
+    print(slowo)
+    numer=[]
+
+    for i in slowo:
+        if i in slownik:
+            numer.append(slownik[i])
+
+    return numer
 
 
-print(szukaj("dwadziescia") + szukaj("siedem"))
+print(szukaj("dwiescie dwadziescia jeden"))
