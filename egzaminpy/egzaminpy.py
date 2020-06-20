3e21d67aceb88200048611b19bc66904995622fa
maslab85 <piotr.p.balcerzak@gmail.com>
2020-06-20 13:17:05 +0200

    Dodane nowe zadania

--- egzaminpy/egzaminpy.py
+++ egzaminpy/egzaminpy.py
@@ -1,3 +1,6 @@
+#Zadania z egzaminu 
+#Opracowane: Piotr Balcerzak
+
 def szybkiesort(lista):
     wieksze_rowne = []
     mniejsze = []
@@ -23,18 +26,23 @@ def unikalne(tekst):
 
 print(unikalne(szybkiesort(lista=[3,1,5,2,3,1,8])))
 
+#do poprawy <--
 
 def wysz_znak(tekst):
     lista=[]
+    temp=0
     for a in list(tekst):
         lista.append(tekst.count(a))
-    for i in range(len(lista)-1):
-        if lista[i]>=lista[i+1]:
-            temp=i
+    for i in range(len(lista)):
+        if lista[i]>=temp:
+            temp=lista[i]
+
 
     return tekst[temp]
 
-print(wysz_znak("ala"))
+print(wysz_znak("ziemniak"))
+
+#-->
 
 def szukaj(liczba):
     slownik = {    "sto":100,
@@ -45,7 +53,7 @@ def szukaj(liczba):
                    "szescset": 600,
                    "siedemset": 700,
                    "osiemset": 800,
-                   "dziewiecet": 90,
+                   "dziewiecet": 900,
                    "dziesiec":10,
                    "dwadziescia": 20,
                    "trzydziesci":30,
@@ -55,6 +63,15 @@ def szukaj(liczba):
                    "siedemdziesiat":70,
                    "osiemdziesiat": 80,
                    "dziewiedziesiat":90,
+                   "jedenascie":11,
+                   "dwanascie":12,
+                   "trzynascie":13,
+                   "czterascie":14,
+                   "pietnascie":15,
+                   "szesnascie":16,
+                   "siedemnascie":17,
+                   "osiemnascie":18,
+                   "dziewietnascie":19,
                    "zero":0,
                    "jeden": 1,
                    "dwa":2,
@@ -86,4 +103,29 @@ def szukaj(liczba):
     return numer
 
 
-print(szukaj("trzysta dwadziescia jeden"))
+print(szukaj("dwiescie dwadziescia jeden"))
+
+def wysz_znak(tekst,znak):
+    liczba=0
+    for a in list(tekst):
+        if znak in a:
+            liczba+=1
+
+
+    return liczba
+
+print(wysz_znak("hakunamatata","a"))
+
+def sort(tab): 
+    for i in range(len(tab)):
+        j=len(tab)-1 
+        while j>i:   
+            if tab[j]>tab[j-1]: 
+                tmp=tab[j]
+                tab[j]=tab[j-1]
+                tab[j-1]=tmp
+            j-=1
+
+    return tab
+
+print(sort([2,5,1,24,7]))
